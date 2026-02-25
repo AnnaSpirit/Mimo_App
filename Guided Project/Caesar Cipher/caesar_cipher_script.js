98ed9024f95d24fe7517ce2e7b0d4f5534abaf2c
AnnaSpirit <annalevy.training@gmail.com>
2026-02-25 14:13:28 +0100

    Caesar Cipher

---
+++ Guided Project/Caesar Cipher/caesar_cipher_script.js
@@ -0,0 +1,24 @@
+function caesarCipher(str, shift) {
+    return str.replace(/[a-zA-Z]/g, function (c) {
+        const base = c < 'a' ? 65 : 97;
+        return String.fromCharCode((c.charCodeAt(0) - base + shift) % 26 + base);
+    });
+}
+
+function translate() {
+    const plaintextValue = document.getElementById('plaintext').value;
+    const shiftValue = parseInt(document.getElementById('shift').value);
+    const outputValue = caesarCipher(plaintextValue, shiftValue);
+    document.getElementById('output').innerHTML = outputValue;
+}
+
+const plaintextInput = document.getElementById('plaintext');
+const shiftInput = document.getElementById('shift');
+
+plaintextInput.addEventListener('input', function () {
+    translate();
+});
+
+shiftInput.addEventListener('input', function () {
+    translate();
+});
