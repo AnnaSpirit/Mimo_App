3c740bbe453147cb904afb0c56dad472a4d3e271
AnnaSpirit <annalevy.training@gmail.com>
2026-07-14 23:45:39 +0300

    Reorganisation _MIMO

---
+++ Guided Project/Données de localisation.py
@@ -0,0 +1,10 @@
+country = "USA"
+state = "Florida"
+city = "Miami"
+continent = "North America"
+country2="Israël"
+state2="NA"
+city2="Raanana"
+continent2="Eurasie"
+
+print("il a voyagé de " + city + " à "+ country2 + " en seulement 10 jours")
---
+++ Guided Project/rock_paper_scissors.py
@@ -0,0 +1,56 @@
+print("Let's play rock, paper, or scissors!")
+
+player_wins = 0
+computer_wins = 0
+
+player_choice = input("Choose rock, paper, or scissors: ").lower()
+
+import random
+# choices = ["rock", "paper", "scissors"]
+
+# computer_choice = random.choice(choices)
+
+# print(f"Computer chose: {computer_choice}")
+
+# if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+#     winner = "Player"
+# elif player_choice == computer_choice:
+#     winner = "Tie"
+# else:
+#     winner = "Computer"
+
+  
+# if winner == "Player":
+#     print("You won!")
+# elif winner == "Computer":
+#     print("Computer won!")
+# else:
+#     print("It's a tie!")
+
+while player_wins < 2 and computer_wins < 2:
+    player_choice = input("Choose rock, paper, or scissors: ").lower()
+    choices = ["rock", "paper, ""scissors"]
+    computer_choice = random.choice(choices)
+    print(f"Computer chose: {computer_choice}")
+    if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+        winner = "Player"
+        player_wins += 1
+    elif player_choice == computer_choice:
+        winner = "Tie"
+    else:
+        winner = "Computer"
+        computer_wins += 1
+
+    if winner == "Player":
+        print("You won!")
+    elif winner == "Computer":
+        print("Computer won!")
+    else:
+        print("It's a tie!")
+
+print(f"Current Score - Player: {player_wins}, Computer: {computer_wins}")
+
+if player_wins > computer_wins:
+    print("Congratulations! You won!")
+else:
+    print("Computer won!")
